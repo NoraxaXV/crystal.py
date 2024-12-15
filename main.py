dff4b4e86aadf4dc1a921b21da758191c585df56
Aaron Smith <kurororbm@gmail.com>
2024-12-15 03:19:06 -0500

    retrieves user profile picture

--- main.py
+++ main.py
@@ -21,7 +21,15 @@ async def ping(ctx):
 
 @bot.command()
 async def crystalize(ctx: commands.Context, *, the_doomed: discord.Member=None):
-    pass
+    if the_doomed == None:
+        try:
+            the_doomed = (await ctx.channel.fetch_message(ctx.message.reference.message_id)).author
+        except:
+            print(f'Crystalization error occured')
+            return
+    await ctx.channel.send(f'Ready to crystalize {the_doomed.display_name}!')
+    await ctx.channel.send(the_doomed.display_avatar.url)
+
 
 token = os.getenv('TOKEN')
 print(f'Authenticating with token {token}')
